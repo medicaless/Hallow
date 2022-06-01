43bad379fca2d83565f089efa76315913ebf5b97
DESKTOP-HTNP8U4\daniel <danil00@naver.com>
2022-06-01 16:44:09 +0900

    Test code print

---
+++ Test Document/main.c
@@ -0,0 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+
+/*
+int main()
+{
+    int numbers[2][2] = {{1,2}, {3,4}};
+    printf("%d\n", numbers[1][0]);
+    return 0;
+}
+*/
+/*
+int main() //
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+    int wow = (a < b ? a:b);
+
+    wow < c ? printf("%d", wow) : printf("%d", c);
+
+    return 0;
+}
+*/
+/*
+int main()
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a % 2 == 0)
+        printf("%d\n",a);
+    if(b % 2 == 0)
+        printf("%d\n",b);
+    if(c % 2 == 0)
+        printf("%d\n",c);
+
+    return 0;
+}
+*/
+/*
+int main() //���� 3���� �Է����� �� ¦�� Ȧ�� ǥ��
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a % 2 == 0)
+        printf("even\n");
+    else
+        printf("odd\n");
+
+    if (b % 2 == 0)
+        printf("even\n");
+    else
+        printf("odd\n");
+
+    if(c % 2 == 0)
+        printf("even\n");
+    else
+        printf("odd\n");
+
+    return 0;
+}
+*/
+/*
+int main()
+{
+    char Tinput;
+
+    scanf("%c", &Tinput);
+
+    switch(Tinput){
+
+    case 'A':
+        printf("%c: best!!", Tinput);
+        break;
+    case 'B':
+        printf("%c: good", Tinput);
+        break;
+    case 'C':
+        printf("%c: run!", Tinput);
+        break;
+    case 'D':
+        printf("%c: slowly~", Tinput);
+        break;
+    default:
+        printf("what?");
+        break;
+    }
+    return 0;
+}
+*/
+/*
+int main()
+{
+    int i, j;
+    int n, m;
+
+    scanf("%d %d", &n, &m);
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=m; j++)
+        {
+            printf("%d %d\n", i, j);
+        }
+    }
+    return 0;
+}
+*/
